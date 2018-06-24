8896a4c73342e59d86d5c3137cfd296269eed5d0
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-06-24 15:02:16 -0400

    keep working on the dnc

--- scratch/layers/dnc/interface.py
+++ scratch/layers/dnc/interface.py
@@ -5,9 +5,13 @@ import torch.nn.functional as functional
 
 
 class Interface:
+    """The interface data structure
+    Takes an interface vector emitted from the controller and splits up into
+    each specific key.
+    """
     def __init__(self, num_read_heads, interface, w):
         self.num_read_heads_ = num_read_heads
-        self.w_ = w
+        self.memory_width = w
         self.read_keys = []
         self.read_strength = []
         self.read_modes = []
@@ -24,42 +28,49 @@ class Interface:
         self.write_gate = self.get_write_gate(interface)
 
     def get_read_key_i_(self, i, interface):
-        return interface[:, i * self.w_:(i + 1) * self.w_]
+        return interface[:, i * self.memory_width:(i + 1) * self.memory_width]
 
     def get_read_strength_i_(self, i, interface):
-        return interface[:, self.num_read_heads_ * self.w_ + i]
+        return interface[:, self.num_read_heads_ * self.memory_width + i]
 
     def get_write_key_(self, interface):
-        start = self.num_read_heads_ * (self.w_ + 1)
-        return interface[:, start:(start + self.w_)]
+        start = self.num_read_heads_ * (self.memory_width + 1)
+        return interface[:, start:(start + self.memory_width)]
 
     def get_write_strength_(self, interface):
-        return interface[:, self.num_read_heads_ * (self.w_ + 1) + self.w_]
+        return interface[:, self.num_read_heads_ * (self.memory_width + 1) +
+                         self.memory_width]
 
     def get_erase_vector_(self, interface):
-        start = self.num_read_heads_ * (self.w_ + 1) + self.w_ + 1
-        return interface[:, start:(start + self.w_)]
+        start = self.num_read_heads_ * (
+            self.memory_width + 1) + self.memory_width + 1
+        return interface[:, start:(start + self.memory_width)]
 
     def get_write_vector_(self, interface):
-        start = self.num_read_heads_ * (self.w_ + 1) + self.w_ * 2 + 1
-        return interface[:, start:(start + self.w_)]
+        start = self.num_read_heads_ * (
+            self.memory_width + 1) + self.memory_width * 2 + 1
+        return interface[:, start:(start + self.memory_width)]
 
     def get_r_free_gates(self, interface):
-        start = self.num_read_heads_ * (self.w_ + 1) + self.w_ * 3 + 1
+        start = self.num_read_heads_ * (
+            self.memory_width + 1) + self.memory_width * 3 + 1
         gates = interface[:, start:(start + self.num_read_heads_)]
         return functional.sigmoid(gates)
 
     def get_allocation_gate(self, interface):
-        start = self.num_read_heads_ * (self.w_ + 2) + self.w_ * 3 + 1
+        start = self.num_read_heads_ * (
+            self.memory_width + 2) + self.memory_width * 3 + 1
         gate = interface[:, start]
         return functional.sigmoid(gate)
 
     def get_write_gate(self, interface):
-        start = self.num_read_heads_ * (self.w_ + 2) + self.w_ * 3 + 2
+        start = self.num_read_heads_ * (
+            self.memory_width + 2) + self.memory_width * 3 + 2
         gate = interface[:, start]
         return functional.sigmoid(gate)
 
     def get_read_modes_i_(self, interface, i):
-        start = self.num_read_heads_ * (self.w_ + 2) + self.w_ * 3 + 3 * i + 3
+        start = self.num_read_heads_ * (
+            self.memory_width + 2) + self.memory_width * 3 + 3 * i + 3
         gates = interface[:, start:(start + 3)]
         return functional.sigmoid(gates)
--- scratch/layers/dnc/memory.py
+++ scratch/layers/dnc/memory.py
@@ -8,37 +8,57 @@ from torch.nn import Module, Parameter
 
 
 class Memory(Module):
+    """The memory module for DNC
+    """
+
     def __init__(self, memory_size, num_read_heads):
-        """
-        Create a memory module. The memory module will be responsible for
-        read write to the memory (The addressing, allocation, temporal link).
-        :param memory_size: The size of the memory (N, W)
-        :param num_read_heads: The number of read heads
+        """Create a memory module.
+
+        The memory module will be responsible for read write to the memory
+        (The addressing, allocation, temporal link).
+
+        Args:
+            memory_size (Tuple[int, int]): The size of the memory
+            num_read_heads (int): The number of read heads
         """
         super().__init__()
         self.memory = Parameter(torch.Tensor(memory_size), requires_grad=False)
-        self.ones = Parameter(torch.ones(memory_size), requires_grad=False)
         self.memory_size = memory_size
         self.num_read_heads = num_read_heads
         self.usage = Parameter(torch.zeros(memory_size[0], 1))
         self.reset_memory()
 
     def reset_memory(self):
+        """Zeros out the memory and usage vector
+        """
         self.memory.data.zero_()
         self.usage.data.zero_()
 
     def addressing_(self, key, weight):
+        """Compute the content based addressing as described in the paper
+
+        Args:
+            key (torch.Tensor): The lookup key. Should be a tensor with shape
+                (1, N)
+            weight (torch.Tensor): The read weight or write weight. Should be a
+                tensor with shape (1, 1)
+
+        Returns:
+            torch.Tensor: The probability for each row to be used.
         """
-        Compute the content based addressing. The result will be
-        :param key: The lookup key. Should be a tensor with shape (1, N)
-        :param weight: A scalar
-        :return: The probability for each row to be used.
-        """
+
         cos_similarity = nn.CosineSimilarity()
         similarity = cos_similarity(key, self.memory)
         return functional.softmax(similarity * weight)
 
     def get_allocation_(self):
+        """Computes the allocation weightings as described in the paper
+
+        Returns:
+            torch.Tensor: The allocation weight for each memory entry (row in
+                the memory matrix)
+        """
+
         _, idx = torch.sort(self.usage)
         allocation = torch.ones_like(self.usage)
         multiply = 1
@@ -48,6 +68,36 @@ class Memory(Module):
         return allocation
 
     def forward(self, interface):
+        """Performs one read and write cycle
+
+        The following steps are deduced from the formulas in the paper:
+            1. Computes the allocation weights
+            2. Computes the weight weights using allocation weights and
+               content weights.
+            3. Write to the memory matrix
+            4. Update the temporal link matrix
+            5. Compute forward and backward weights
+            6. Compute the read weight
+            7. Perform read
+            8. Update usage vector
+
+        Note that we first perform the write then perform the read. This is
+        because in the paper read weight depends on the current temporal linkage
+        matrix which can only be updated after write.
+
+        Another notice is that the interface vectors here are all row vectors,
+        unlike in the paper
+
+        Args:
+            interface (Interface): The interface emitted from the controller
+        """
+        allocation_weight = self.get_allocation_()
         write_content_address = self.addressing_(interface.write_key,
                                                  interface.write_strength)
+        write_weights = interface.write_gate * (
+            interface.allocation_gate * allocation_weight +
+            (1 - interface.allocation_gate) * write_content_address)
 
+        # Perform the write
+
+        self.memory = self.memory * (1 - )
