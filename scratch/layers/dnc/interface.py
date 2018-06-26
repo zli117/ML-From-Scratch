9aa0e3dbc133df2662910b4d7a38f18e09f3a0e8
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-06-25 22:17:24 -0400

    still working on the memory of dnc

--- scratch/layers/dnc/interface.py
+++ scratch/layers/dnc/interface.py
@@ -9,68 +9,75 @@ class Interface:
     Takes an interface vector emitted from the controller and splits up into
     each specific key.
     """
-    def __init__(self, num_read_heads, interface, w):
-        self.num_read_heads_ = num_read_heads
-        self.memory_width = w
-        self.read_keys = []
-        self.read_strength = []
-        self.read_modes = []
-        for i in range(num_read_heads):
-            self.read_keys.append(self.get_read_key_i_(i, interface))
-            self.read_strength.append(self.get_read_strength_i_(i, interface))
-            self.read_modes.append(self.get_read_modes_i_(i, interface))
-        self.write_key = self.get_write_key_(interface)
-        self.write_strength = self.get_write_strength_(interface)
-        self.erase_vector = self.get_erase_vector_(interface)
-        self.write_vector = self.get_write_vector_(interface)
-        self.free_gates = self.get_r_free_gates(interface)
-        self.allocation_gate = self.get_allocation_gate(interface)
-        self.write_gate = self.get_write_gate(interface)
+    def __init__(self, num_read_heads, interface, cell_width):
+        self.num_read_heads = num_read_heads
+        self.cell_width = cell_width
+        self.read_keys = self.get_read_keys_(interface) # (B, #RH, W)
+        self.read_strength = self.get_read_strengths_(interface) # (B, #RH, 1)
+        self.read_modes = self.get_read_modes_(interface) # (B, #RH, 3)
+        self.write_key = self.get_write_key_(interface) # (B, W)
+        self.write_strength = self.get_write_strength_(interface) # (B, 1)
+        self.erase_vector = self.get_erase_vector_(interface) # (B, 1, W)
+        self.write_vector = self.get_write_vector_(interface) # (B, 1, W)
+        self.free_gates = self.get_r_free_gates(interface) # (B, #RH, 1)
+        self.allocation_gate = self.get_allocation_gate(interface) # (B, 1, 1)
+        self.write_gate = self.get_write_gate(interface) # (B, 1, 1)
 
-    def get_read_key_i_(self, i, interface):
-        return interface[:, i * self.memory_width:(i + 1) * self.memory_width]
+    def get_read_keys_(self, interface):
+        keys = interface[:, :self.num_read_heads * self.cell_width]
+        return keys.unsqueeze(dim=0)
 
-    def get_read_strength_i_(self, i, interface):
-        return interface[:, self.num_read_heads_ * self.memory_width + i]
+    def get_read_strengths_(self, interface):
+        start = self.num_read_heads * self.cell_width
+        strengths = interface[:, start:start + self.num_read_heads]
+        return strengths.unsqueeze(dim=2)
 
     def get_write_key_(self, interface):
-        start = self.num_read_heads_ * (self.memory_width + 1)
-        return interface[:, start:(start + self.memory_width)]
+        start = self.num_read_heads * (self.cell_width + 1)
+        return interface[:, start:(start + self.cell_width)]
 
     def get_write_strength_(self, interface):
-        return interface[:, self.num_read_heads_ * (self.memory_width + 1) +
-                         self.memory_width]
+        return interface[:, self.num_read_heads * (self.cell_width + 1) +
+                         self.cell_width]
 
     def get_erase_vector_(self, interface):
-        start = self.num_read_heads_ * (
-            self.memory_width + 1) + self.memory_width + 1
-        return interface[:, start:(start + self.memory_width)]
+        start = self.num_read_heads * (
+            self.cell_width + 1) + self.cell_width + 1
+        erase_vector = interface[:, start:(start + self.cell_width)]
+        return erase_vector.unsqueeze(dim=1)
 
     def get_write_vector_(self, interface):
-        start = self.num_read_heads_ * (
-            self.memory_width + 1) + self.memory_width * 2 + 1
-        return interface[:, start:(start + self.memory_width)]
+        start = self.num_read_heads * (
+            self.cell_width + 1) + self.cell_width * 2 + 1
+        write_vector = interface[:, start:(start + self.cell_width)]
+        return write_vector.unsqueeze(dim=1)
 
     def get_r_free_gates(self, interface):
-        start = self.num_read_heads_ * (
-            self.memory_width + 1) + self.memory_width * 3 + 1
-        gates = interface[:, start:(start + self.num_read_heads_)]
-        return functional.sigmoid(gates)
+        start = self.num_read_heads * (
+            self.cell_width + 1) + self.cell_width * 3 + 1
+        gates = interface[:, start:(start + self.num_read_heads)]
+        return functional.sigmoid(gates).unsqueeze(dim=2)
 
     def get_allocation_gate(self, interface):
-        start = self.num_read_heads_ * (
-            self.memory_width + 2) + self.memory_width * 3 + 1
+        start = self.num_read_heads * (
+            self.cell_width + 2) + self.cell_width * 3 + 1
         gate = interface[:, start]
-        return functional.sigmoid(gate)
+        return functional.sigmoid(gate).unsqueeze(dim=2)
 
     def get_write_gate(self, interface):
-        start = self.num_read_heads_ * (
-            self.memory_width + 2) + self.memory_width * 3 + 2
+        start = self.num_read_heads * (
+            self.cell_width + 2) + self.cell_width * 3 + 2
         gate = interface[:, start]
-        return functional.sigmoid(gate)
+        return functional.sigmoid(gate).unsqueeze(dim=2)
 
-    def get_read_modes_i_(self, interface, i):
-        start = self.num_read_heads_ * (
-            self.memory_width + 2) + self.memory_width * 3 + 3 * i + 3
-        gates = interface[:, start:(start + 3)]
-        return functional.sigmoid(gates)
+    def get_read_modes_(self, interface):
+        """Extracts the read modes from the interface for all batches
+
+        Args:
+            interface (torch.Tensor): interface vector
+
+        Returns:
+            torch.Tensor: shape: (B, #RH, 3)
+        """
+        modes = interface[:, -3 * self.num_read_heads]
+        return modes.view(-1, self.num_read_heads, 3)
--- scratch/layers/dnc/memory.py
+++ scratch/layers/dnc/memory.py
@@ -5,13 +5,16 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as functional
 from torch.nn import Module, Parameter
+from torch.autograd import Variable
 
 
 class Memory(Module):
     """The memory module for DNC
+
+    The memory module is only for one data point in a batch.
     """
 
-    def __init__(self, memory_size, num_read_heads):
+    def __init__(self, memory_size, num_read_heads, batch_size):
         """Create a memory module.
 
         The memory module will be responsible for read write to the memory
@@ -20,36 +23,92 @@ class Memory(Module):
         Args:
             memory_size (Tuple[int, int]): The size of the memory
             num_read_heads (int): The number of read heads
+            batch_size (int): The batch size
         """
         super().__init__()
-        self.memory = Parameter(torch.Tensor(memory_size), requires_grad=False)
+        num_cells, cell_size = memory_size
         self.memory_size = memory_size
         self.num_read_heads = num_read_heads
-        self.usage = Parameter(torch.zeros(memory_size[0], 1))
-        self.reset_memory()
+        self.batch_size = batch_size
+        self.memory = Parameter(
+            torch.Tensor((batch_size, num_cells, cell_size)),
+            requires_grad=False)
+        self.temporal_link = Parameter(
+            torch.Tensor((batch_size, num_cells, num_cells)),
+            requires_grad=False)
+        self.usage = Parameter(
+            torch.Tensor(batch_size, 1, num_cells), requires_grad=False)
+        self.allocation = Parameter(
+            torch.Tensor(batch_size, 1, num_cells), requires_grad=False)
+        self.precedence = Parameter(
+            torch.Tensor(batch_size, 1, num_cells), requires_grad=False)
+        self.read_weights = Parameter(
+            torch.Tensor(batch_size, num_read_heads, num_cells),
+            requires_grad=False)
+        self.reset()
 
-    def reset_memory(self):
-        """Zeros out the memory and usage vector
+    def reset(self):
+        """Resets the memory module
+
+        Zeros out memory, usage vector, temporal links, allocation vector,
+        precedence vector and read weights.
         """
         self.memory.data.zero_()
         self.usage.data.zero_()
+        self.temporal_link.zero_()
+        self.allocation.zero_()
+        self.precedence.zero_()
+        self.read_weights.zero_()
 
-    def addressing_(self, key, weight):
+    def write_addressing_(self, key, weight):
         """Compute the content based addressing as described in the paper
 
         Args:
-            key (torch.Tensor): The lookup key. Should be a tensor with shape
-                (1, N)
-            weight (torch.Tensor): The read weight or write weight. Should be a
-                tensor with shape (1, 1)
+            key (torch.Tensor): The write key. Should be a tensor with shape
+                (Batch, W)
+            weight (torch.Tensor): The write weight. A tensor with shape
+                (Batch, 1)
 
         Returns:
-            torch.Tensor: The probability for each row to be used.
+            torch.Tensor: The probability for each row to be written. The shape
+                is (Batch, 1, W)
         """
-
         cos_similarity = nn.CosineSimilarity()
-        similarity = cos_similarity(key, self.memory)
-        return functional.softmax(similarity * weight)
+        key = key.unsqueeze(dim=1)
+        similarities = [
+            functional.softmax(
+                cos_similarity(key[i], self.memory[i]).unsqueeze(dim=0) *
+                weight[i],
+                dim=0) for i in range(self.batch_size)
+        ]
+        return torch.stack(similarities, dim=0)
+
+    def read_addressing_(self, key, weight):
+        """Computes the read content addressing for the whole batch
+
+        Args:
+            key (torch.Tensor): The read keys for all batches for all read
+                heads. Shape should be (Batch, #RH, W)
+            weight (torch.Tensor): Shape should be (Batch, #RH, 1)
+
+        Returns:
+            torch.Tensor: The probability for each row to be read by each read
+                head. The shape is (Batch, #RH, W)
+        """
+        cos_similarity = nn.CosineSimilarity()
+        key = key.unsqueeze(dim=2)
+        addressing = []
+        for i in range(self.batch_size):
+            batch_key = key[i]
+            batch_weight = weight[i]
+            similarities = [
+                functional.softmax(
+                    cos_similarity(batch_key[j], self.memory[i]), dim=0)
+                for j in range(self.num_read_heads)
+            ]
+            similarities = torch.stack(similarities, dim=0) * batch_weight
+            addressing.append(similarities)
+        return torch.stack(addressing, dim=0)
 
     def get_allocation_(self):
         """Computes the allocation weightings as described in the paper
@@ -58,14 +117,15 @@ class Memory(Module):
             torch.Tensor: The allocation weight for each memory entry (row in
                 the memory matrix)
         """
+        _, idx = torch.sort(self.usage, dim=2)
+        multiply = Variable(self.usage.new_ones(self.batch_size, 1))
+        for i in range(idx.shape[2]):
+            self.allocation[:, :, i] = (1 - self.usage[:, :, i]) * multiply
+            multiply *= self.usage[:, :, i]
+        return self.allocation
 
-        _, idx = torch.sort(self.usage)
-        allocation = torch.ones_like(self.usage)
-        multiply = 1
-        for i in idx:
-            allocation[i][0] = (1 - self.usage[i][0]) * multiply
-            multiply *= self.usage[i, 0]
-        return allocation
+    def get_read_weight_(self, i, interface):
+        pass
 
     def forward(self, interface):
         """Performs one read and write cycle
@@ -76,10 +136,10 @@ class Memory(Module):
                content weights.
             3. Write to the memory matrix
             4. Update the temporal link matrix
+            8. Update usage vector
             5. Compute forward and backward weights
             6. Compute the read weight
             7. Perform read
-            8. Update usage vector
 
         Note that we first perform the write then perform the read. This is
         because in the paper read weight depends on the current temporal linkage
@@ -92,12 +152,46 @@ class Memory(Module):
             interface (Interface): The interface emitted from the controller
         """
         allocation_weight = self.get_allocation_()
-        write_content_address = self.addressing_(interface.write_key,
-                                                 interface.write_strength)
-        write_weights = interface.write_gate * (
-            interface.allocation_gate * allocation_weight +
-            (1 - interface.allocation_gate) * write_content_address)
+        write_content_address = self.write_addressing_(interface.write_key,
+                                                       interface.write_strength)
+
+        write_weights = (
+            interface.allocation_gate *
+            (allocation_weight - write_content_address) + write_content_address)
+        write_weights *= interface.write_gate
 
         # Perform the write
 
-        self.memory = self.memory * (1 - )
+        write_vector = interface.write_vector
+        erase_vector = interface.erase_vector
+        transposed_write_weights = torch.transpose(write_weights, 1, 2)
+
+        self.memory *= 1 - torch.bmm(transposed_write_weights, erase_vector)
+        self.memory += torch.bmm(transposed_write_weights, write_vector)
+
+        # Update the temporal linkage
+
+        num_cells = self.memory_size[0]
+        grid_sum = (transposed_write_weights.repeat(1, 1, num_cells) +
+                    write_weights.repeat(1, num_cells, 1))
+        grid_subtract = 1 - grid_sum
+        self.temporal_link = (grid_subtract * self.temporal_link + torch.bmm(
+            transposed_write_weights, self.precedence))
+        self.precedence = (
+            (1 - torch.sum(write_weights)) * self.precedence + write_weights)
+
+        # Update usage vector
+
+        retention_vector = Variable(
+            self.usage.new_ones(self.batch_size, 1, num_cells))
+        free_gates = interface.free_gates
+        old_read_weights = self.read_weights
+        for i in range(self.num_read_heads):
+            retention_vector *= (
+                1 - free_gates[:, i, :] * old_read_weights[:, i, :]
+            ).unsqueeze(dim=1)
+        old_usage = self.usage
+        self.usage = (old_usage + write_weights -
+                      old_usage * write_weights) * retention_vector
+
+        # Read
