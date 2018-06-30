580c7373ca3efff718e6acdc26263112d130c2d3
Zonlgin-Li6565 <development.my6565@gmail.com>
2018-06-30 16:36:24 -0400

    test for interface

--- scratch/layers/dnc/interface.py
+++ scratch/layers/dnc/interface.py
@@ -9,19 +9,20 @@ class Interface:
     Takes an interface vector emitted from the controller and splits up into
     each specific key.
     """
+
     def __init__(self, num_read_heads, interface, cell_width):
         self.num_read_heads = num_read_heads
         self.cell_width = cell_width
-        self.read_keys = self.get_read_keys_(interface) # (B, #RH, W)
-        self.read_strength = self.get_read_strengths_(interface) # (B, #RH, 1)
-        self.read_modes = self.get_read_modes_(interface) # (B, #RH, 3)
-        self.write_key = self.get_write_key_(interface) # (B, W)
-        self.write_strength = self.get_write_strength_(interface) # (B, 1)
-        self.erase_vector = self.get_erase_vector_(interface) # (B, 1, W)
-        self.write_vector = self.get_write_vector_(interface) # (B, 1, W)
-        self.free_gates = self.get_r_free_gates(interface) # (B, #RH, 1)
-        self.allocation_gate = self.get_allocation_gate(interface) # (B, 1, 1)
-        self.write_gate = self.get_write_gate(interface) # (B, 1, 1)
+        self.read_keys = self.get_read_keys_(interface)  # (B, #RH, W)
+        self.read_strength = self.get_read_strengths_(interface)  # (B, #RH, 1)
+        self.read_modes = self.get_read_modes_(interface)  # (B, #RH, 3)
+        self.write_key = self.get_write_key_(interface)  # (B, W)
+        self.write_strength = self.get_write_strength_(interface)  # (B, 1)
+        self.erase_vector = self.get_erase_vector_(interface)  # (B, 1, W)
+        self.write_vector = self.get_write_vector_(interface)  # (B, 1, W)
+        self.free_gates = self.get_r_free_gates(interface)  # (B, #RH, 1)
+        self.allocation_gate = self.get_allocation_gate(interface)  # (B, 1, 1)
+        self.write_gate = self.get_write_gate(interface)  # (B, 1, 1)
 
     def get_read_keys_(self, interface):
         keys = interface[:, :self.num_read_heads * self.cell_width]
@@ -41,32 +42,32 @@ class Interface:
                          self.cell_width].unsqueeze(dim=1)
 
     def get_erase_vector_(self, interface):
-        start = self.num_read_heads * (
-            self.cell_width + 1) + self.cell_width + 1
+        start = (
+            self.num_read_heads * (self.cell_width + 1) + self.cell_width + 1)
         erase_vector = interface[:, start:(start + self.cell_width)]
         return erase_vector.unsqueeze(dim=1)
 
     def get_write_vector_(self, interface):
-        start = self.num_read_heads * (
-            self.cell_width + 1) + self.cell_width * 2 + 1
+        start = (self.num_read_heads * (self.cell_width + 1) +
+                 self.cell_width * 2 + 1)
         write_vector = interface[:, start:(start + self.cell_width)]
         return write_vector.unsqueeze(dim=1)
 
     def get_r_free_gates(self, interface):
-        start = self.num_read_heads * (
-            self.cell_width + 1) + self.cell_width * 3 + 1
+        start = (self.num_read_heads * (self.cell_width + 1) +
+                 self.cell_width * 3 + 1)
         gates = interface[:, start:(start + self.num_read_heads)]
         return functional.sigmoid(gates).unsqueeze(dim=2)
 
     def get_allocation_gate(self, interface):
-        start = self.num_read_heads * (
-            self.cell_width + 2) + self.cell_width * 3 + 1
+        start = (self.num_read_heads * (self.cell_width + 2) +
+                 self.cell_width * 3 + 1)
         gate = interface[:, start].unsqueeze(dim=1)
         return functional.sigmoid(gate).unsqueeze(dim=2)
 
     def get_write_gate(self, interface):
-        start = self.num_read_heads * (
-            self.cell_width + 2) + self.cell_width * 3 + 2
+        start = (self.num_read_heads * (self.cell_width + 2) +
+                 self.cell_width * 3 + 2)
         gate = interface[:, start].unsqueeze(dim=1)
         return functional.sigmoid(gate).unsqueeze(dim=2)
 
@@ -80,4 +81,5 @@ class Interface:
             torch.Tensor: shape: (B, #RH, 3)
         """
         modes = interface[:, -3 * self.num_read_heads:]
-        return modes.view(-1, self.num_read_heads, 3)
+        modes = modes.view(-1, self.num_read_heads, 3)
+        return functional.softmax(modes, dim=2)
--- scratch/test/dnc_interface_test.py
+++ scratch/test/dnc_interface_test.py
@@ -4,6 +4,7 @@ import random
 import unittest
 
 import torch
+from torch.nn import functional
 
 from scratch.layers.dnc.interface import Interface
 
@@ -26,52 +27,104 @@ class TestDNCInterface(unittest.TestCase):
         self.interface = Interface(self.num_read_heads, self.data,
                                    self.cell_width)
 
+    def helper(self,
+               start,
+               span,
+               batch,
+               sigmoid_activate=False,
+               read_modes=False):
+        first_batch = batch[0].view(1, -1)
+        target = torch.arange(start, start + span)
+        if sigmoid_activate:
+            target = functional.sigmoid(target)
+        if read_modes:
+            target = target.view(-1, 3)
+            target = functional.softmax(target, dim=-1)
+            target = target.view(1, -1)
+        self.assertEqual(torch.sum(first_batch != target), 0)
+
     def test_read_keys(self):
         read_keys = self.interface.read_keys
         self.assertEqual(
             read_keys.shape,
             (self.batch_size, self.num_read_heads, self.cell_width))
+        start = 0
+        span = self.num_read_heads * self.cell_width
+        self.helper(start, span, read_keys)
 
     def test_read_strengths(self):
         read_strength = self.interface.read_strength
         self.assertEqual(read_strength.shape,
                          (self.batch_size, self.num_read_heads, 1))
-
-    def test_read_modes(self):
-        read_modes = self.interface.read_modes
-        self.assertEqual(read_modes.shape,
-                         (self.batch_size, self.num_read_heads, 3))
+        start = self.num_read_heads * self.cell_width
+        span = self.num_read_heads
+        self.helper(start, span, read_strength)
 
     def test_write_key(self):
         write_key = self.interface.write_key
         self.assertEqual(write_key.shape, (self.batch_size, self.cell_width))
+        start = self.num_read_heads * (self.cell_width + 1)
+        span = self.cell_width
+        self.helper(start, span, write_key)
 
     def test_write_strength(self):
         write_strength = self.interface.write_strength
         self.assertEqual(write_strength.shape, (self.batch_size, 1))
+        start = self.num_read_heads * (self.cell_width + 1) + self.cell_width
+        span = 1
+        self.helper(start, span, write_strength)
 
     def test_erase_vector(self):
         erase_vector = self.interface.erase_vector
         self.assertEqual(erase_vector.shape,
                          (self.batch_size, 1, self.cell_width))
+        start = (
+            self.num_read_heads * (self.cell_width + 1) + self.cell_width + 1)
+        span = self.cell_width
+        self.helper(start, span, erase_vector)
 
     def test_write_vector(self):
         write_vector = self.interface.write_vector
         self.assertEqual(write_vector.shape,
                          (self.batch_size, 1, self.cell_width))
+        start = (self.num_read_heads * (self.cell_width + 1) +
+                 self.cell_width * 2 + 1)
+        span = self.cell_width
+        self.helper(start, span, write_vector)
 
     def test_free_gates(self):
         free_gates = self.interface.free_gates
         self.assertEqual(free_gates.shape,
                          (self.batch_size, self.num_read_heads, 1))
+        start = (self.num_read_heads * (self.cell_width + 1) +
+                 self.cell_width * 3 + 1)
+        span = self.num_read_heads
+        self.helper(start, span, free_gates, sigmoid_activate=True)
 
     def test_allocation_gate(self):
         allocation_gate = self.interface.allocation_gate
         self.assertEqual(allocation_gate.shape, (self.batch_size, 1, 1))
+        start = (self.num_read_heads * (self.cell_width + 2) +
+                 self.cell_width * 3 + 1)
+        span = 1
+        self.helper(start, span, allocation_gate, sigmoid_activate=True)
 
     def test_write_gate(self):
         write_gate = self.interface.write_gate
         self.assertEqual(write_gate.shape, (self.batch_size, 1, 1))
+        start = (self.num_read_heads * (self.cell_width + 2) +
+                 self.cell_width * 3 + 2)
+        span = 1
+        self.helper(start, span, write_gate, sigmoid_activate=True)
+
+    def test_read_modes(self):
+        read_modes = self.interface.read_modes
+        self.assertEqual(read_modes.shape,
+                         (self.batch_size, self.num_read_heads, 3))
+        start = (self.num_read_heads * (self.cell_width + 2) +
+                 self.cell_width * 3 + 3)
+        span = self.num_read_heads * 3
+        self.helper(start, span, read_modes, read_modes=True)
 
 
 if __name__ == '__main__':
